0b734018c83237d0830b7e82e7013ffac516a550
Chris Mitchell <chris.mit7@gmail.com>
2015-04-15 17:29:32 -0400

    task views added

--- djguihome/urls.py
+++ djguihome/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from .views import DjanguiHomeView
 
 urlpatterns = [
-    url(r'^', DjanguiHomeView.as_view(), name='djangui_home'),
+    url(r'^$', DjanguiHomeView.as_view(), name='djangui_home'),
 ]
